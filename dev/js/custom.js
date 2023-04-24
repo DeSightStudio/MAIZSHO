4e11d91e2ecdb49a098b600a5e43cf1bec4181d6
DeSightStudio <hello@desightstudio.com>
2023-04-25 00:07:37 +0200

    Install dev envoirenment

---
+++ dev/js/custom.js
@@ -0,0 +1,54 @@
+/**
+ * Include your custom JavaScript here.
+ *
+ * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
+ * changes on product page, you can attach a listener to the document:
+ *
+ * document.addEventListener('variant:changed', function(event) {
+ *   var variant = event.detail.variant; // Gives you access to the whole variant details
+ * });
+ *
+ * You can also add a listener whenever a product is added to the cart:
+ *
+ * document.addEventListener('product:added', function(event) {
+ *   var variant = event.detail.variant; // Get the variant that was added
+ *   var quantity = event.detail.quantity; // Get the quantity that was added
+ * });
+ *
+ * If you just want to force refresh the mini-cart without adding a specific product, you can trigger the event
+ * "cart:refresh" in a similar way (in that case, passing the quantity is not necessary):
+ *
+ * document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
+ *   bubbles: true
+ * }));
+ */
+
+var ready = function(callback) {
+  if (document.readyState != 'loading') callback();
+  else document.addEventListener('DOMContentLoaded', callback);
+}
+
+ready(function() {
+    var bodyEl = document.body;
+
+    if(bodyEl.classList.contains('template-product')) {
+
+      var el = document.querySelector('.mod-stickybar-cart');
+
+      window.addEventListener('scroll', function() {
+
+        var scrolled = document.scrollingElement.scrollTop;
+        var position = el.offsetTop;
+        
+        if(scrolled > 350) {
+          el.classList.add('fade-in');
+        } else {
+          el.classList.remove('fade-in');
+        }
+      });
+	}
+  
+  $('.ProductForm__Label:contains("Menge"), .ProductForm__QuantitySelector').wrapAll('<div class="wrapped" />');
+  
+});
+
---
+++ dev/js/essentials.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+	
+	setTimeout(function(){ 
+	
+		/*jQuery(".page main.content-for-layout p:has(img)").addClass("paragraph__image-container");
+		jQuery(".page main.content-for-layout p:has(img)").closest('p').next().addClass("paragraph__image-container__after");
+		jQuery(".page main.content-for-layout p:has(img)").closest('p').next().closest('p').next().addClass("paragraph__image-container__after__clear");
+		
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > 1700){
+			  $('._12n8yed').addClass('scroll');
+			} else {
+			  $('._12n8yed').removeClass('scroll');
+			}
+		  });*/
+		  
+		  setTimeout(function(){  
+			  
+			  jQuery('.open-pop-up').magnificPopup({
+				  type:'inline',
+				  removalDelay: 500, //delay removal by X to allow out-animation
+				  callbacks: {
+					  beforeOpen: function() {
+						  this.st.mainClass = this.st.el.attr('data-effect');
+					  }
+				  }
+			  });
+		  
+		  }, 100);
+		
+	}, 100);
+	
+});
